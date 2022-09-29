792b30b60ba9a993b33103d8910af996bbd3d6a1
Simo Liuke 2 <simo.liuke@gmail.com>
2022-09-29 10:51:04 +0300

    v.1.1

--- exercises.py
+++ exercises.py
@@ -2,33 +2,6 @@ import math
 import mysql.connector
 import sys
 
-# 8.1
-
-connection = mysql.connector.connect(
-    host='127.0.0.1',
-    port='3306',
-    database='flight_game',
-    user='root',
-    password='1234',
-    autocommit=True
-    )
-
-
-def fetch(name, location):
-    sql = "SELECT gps_code FROM airport"
-    sql += " WHERE Last_name='" + last_name + "'"
-    print(sql)
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    if cursor.rowcount > 0:
-        for row in result:
-            print(f"Hello! I'm {row[2]} {row[1]}. My salary is {row[3]} euros per month.")
-
-
-code = input("Enter IACO code:")
-print(name, location)
-
 
 sys.exit(0)
 
