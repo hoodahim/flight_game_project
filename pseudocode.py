7c210d7ff730faea2c712144702ba1c92ea03d82
Simo Liuke 2 <simo.liuke@gmail.com>
2022-09-29 10:47:29 +0300

    v.1

---
+++ pseudocode.py
@@ -0,0 +1,28 @@
+import mysql.connector
+import sys
+import random
+
+
+def randomizer():
+    random.randrange(1, 1000000)  # replace with real numbers
+    # use database position numbers choose the airport
+    return ()
+
+
+start = randomizer()
+final = randomizer()
+
+while start != final:
+    confirm = "n"
+    change = input("Enter airport to fly to")
+    # check if name or other data entered is in database
+    # if in print out airport info
+    # if no re-loop
+
+    confirm = input("Are you sure (y/n)")
+    if confirm == "y":
+        start = change
+    else:
+        print("")
+
+print("You have reached your destination.")
