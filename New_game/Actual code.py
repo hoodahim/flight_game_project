abc139dfc8b7c9e058ed1139b69ec085cfd01f59
VlazaIT <vladislav.zakatov@metropolia.fi>
2022-10-04 14:08:45 +0300

    Game rules coding added

--- New_game/Actual code.py
+++ New_game/Actual code.py
@@ -2,10 +2,10 @@ import sys
 import mysql.connector
 
 
-# functions
-def location_finder(icao):
+# Function to find the airport based on entered ICAO code (by user) and provide with airport's name & municipality
+def location_finder(ICAO):
     cursor = connection.cursor()
-    cursor.execute("SELECT ident, name, municipality FROM airport WHERE ident='" + icao + "'")
+    cursor.execute("SELECT ident, name, municipality FROM airport WHERE ident='" + ICAO + "'")
     result = cursor.fetchall()
     if cursor.rowcount > 0:
         for row in result:
@@ -14,6 +14,7 @@ def location_finder(icao):
             return airport, municipality
 
 
+# Function to print the airport's information based on country code entered by user
 def airport_printer(country_code):
     cursor = connection.cursor()
     cursor.execute("SELECT ident, type, name, iso_country FROM airport "
@@ -21,41 +22,46 @@ def airport_printer(country_code):
     result = cursor.fetchall()
     if cursor.rowcount > 0:
         for row in result:
-            print(f"{row[2]} is a {row[1]} type of airport which icao code is {row[0]}.")
+            print(f"{row[2]} is a {row[1]} type of airport which ICAO code is {row[0]}.")
     return
 
 
 # main game
-# NOTE: IT'S DIFFERENT FOR EVERYONE
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port=3306,
-         database='cars',
-         user='root',
-         password='MariaDB',
+         database='cars', # NOTE: IT'S DIFFERENT FOR EVERYONE (Now it's Vlad's version)
+         user='root', # NOTE: IT'S DIFFERENT FOR EVERYONE (Now it's Vlad's version)
+         password='MariaDB', # NOTE: IT'S DIFFERENT FOR EVERYONE (Now it's Vlad's version)
          autocommit=True
          )
 
+ICAO_code_input = input("Enter ICAO code of an airport: ")
+location_finder(ICAO_code_input)
+
+Country_code_input = input("Enter country's code: ")
+airport_printer(Country_code_input)
+
 screen_name = input("Please choose a username: ")
-print(f"Hello, {screen_name} and welcome to theNameOfTheGame. It looks like you are currently in Helsinki, Finland.")
-print(""" Let me tell you a bit more about the game:
-You have had this bucket list for a while now and you decided it's time to put things finally into motion.
-Based on your calculation you have overall 1200 euro to visit 5 locations in Europe.
-You wish that all these 5 location will each have a different weather condition to the ones you have already visited. 
-Before flying off to your first adventure, I ask you kindly to take into consideration sustainability 
-and plan your trip ahead to minimalize unnecessary CO2 emission.
+print(f"Hello, {screen_name}, and welcome to \033[1m" + "theNameOfTheGame" + "\033[0m. It looks like you are currently in Helsinki, Finland.\n")
+print("""Let me tell you a bit more about the game: \n
+You have had this bucket list for a while now and you decided it's time to put things finally into motion!
+- Based on your calculation you have overall 1200 EUR to visit 5 locations in Europe.
+- You wish that all these 5 locations will each have a different weather condition compared to the ones you have already visited. 
+- Before flying off to your first adventure, I ask you kindly to take into consideration sustainability 
+  and plan your trip ahead to minimalize unnecessary CO2 emission.
 """)
 
-# ask player BY NAME if all is clear and ready to see the goals use .lower() at the end
-# while answer is not "yes":
-    # if answer is "no":
-    #   print our the let me tell you part with slight change.
-    #   ask player if all is clear and ready to see the goals use .lower() at the end
-    # else:
-    #   print: invalid input and ask if all is clear and ready to see goals
+# print(f"Are the rules clear to you and you are ready to see the goals, {screen_name} (yes/no)?")
+answer = input(f"Are the rules clear to you, and you are ready to see the goals, {screen_name} (yes/no)? ").lower()
 
-#print(""" #put the goals here
-# """)
+while answer != "yes":
+    if answer == "no":
+        print("Let me tell you part with slight change: \nMORE DETAILED DESCRIPTION")
+        answer = input("").lower()
+    else:
+        print("Invalid input, are you ready to see the goals?")
+        answer = input("").lower()
 
 # ask player BY NAME if all is clear and ready to choose first location, use .lower() at the end
 # while answer is not "yes":
