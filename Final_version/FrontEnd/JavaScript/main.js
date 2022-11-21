3eddc17c9907589144df4ae096ab71db66bb58ad
Gemmus <decadentcrown@gmail.com>
2022-11-21 11:54:09 +0200

    Initial upload.

---
+++ Final_version/FrontEnd/JavaScript/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const map = L.map('map', {tap: false});
+L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+  maxZoom: 20,
+  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+}).addTo(map);
+map.setView([60, 24], 7);
+
+//form for player name
+document.querySelector('#player-form').addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  const playerName = document.querySelector('#player-input').value;
+  document.getElementById('replace-name').innerHTML = playerName
+  document.querySelector('#player-modal').classList.add('hide');
+});
